14fe413d0d54440048fdb37878a1523a3c91b23b
Lazaro Mbaila <lazarozito@gmail.com>
2025-03-19 11:38:36 +0200

    strutucture request oppening

--- router/router.go
+++ router/router.go
@@ -4,10 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func Initialize() {
 	router := gin.Default()
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run("8080")
+
+	initializeRoutes(router)
+
+	router.Run()
 }
